a6481fe1145e77c831ee49f0419bdd0b515de4c3
Magnus Egeland <magnus.egeland@gmail.com>
2025-04-01 11:57:43 +0200

    script for converting several files

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -8,4 +8,3 @@ xml2json("../Webis-data/extracted/articles-training-byarticle-20181122.xml","dat
 xml2json("../Webis-data/extracted/articles-test-byarticle-20181207.xml","data/articles-test-byarticle.jsonl")
 xml2json("../Webis-data/extracted/articles-training-bypublisher-20181122.xml","data/articles-training-bypublisher.jsonl")
 xml2json("../Webis-data/extracted/articles-test-bypublisher-20181212.xml","data/articles-test-bypublisher.jsonl")
-
