bcdf55b104a64b7d079268f4a3e9fbf294b6421e
Magnus Egeland <magnus.egeland@gmail.com>
2025-04-01 11:57:43 +0200

    script for converting several files

---
+++ preprocessing/preprocess.py
@@ -0,0 +1,9 @@
+import os
+from xml2json import xml2json
+
+os.makedirs("data",exist_ok=True)
+
+xml2json("../Webis-data/data/articles-training-byarticle-20181122.xml","data/articles-training-byarticle.jsonl")
+xml2json("../Webis-data/data/articles-test-byarticle-20181207.xml","data/articles-test-byarticle.jsonl")
+xml2json("../Webis-data/data/articles-training-bypublisher-20181122.xml","data/articles-training-bypublisher.jsonl")
+xml2json("../Webis-data/data/articles-test-bypublisher-20181212.xml","data/articles-test-bypublisher.jsonl")
