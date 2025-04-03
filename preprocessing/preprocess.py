14984d978ba889e4ca3d0998444ac58f7483d4d7
Magnus Egeland <93578872+Mekruba@users.noreply.github.com>
2025-04-03 09:24:43 +0200

    CNN approach description (#2)
    * update ignore file
    
    * (feat)Preprocessing: Created a XML to json converter
    
    * Script for download of data from Webis.de
    
    * (fix): move timing into converter function
    
    * script for converting several files
    
    * more updates
    
    * Method of hyperpartizan news detection using CNN
    
    * update ignore file
    
    * Script for download of data from Webis.de
    
    * script for converting several files
    
    * (feat)Preprocessing: Created a XML to json converter
    
    * (fix): move timing into converter function
    
    * update download script to extract data

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -3,6 +3,7 @@ from xml2json import xml2json
 
 os.makedirs("data",exist_ok=True)
 
+
 xml2json("../Webis-data/extracted/articles-training-byarticle-20181122.xml","data/articles-training-byarticle.jsonl")
 xml2json("../Webis-data/extracted/articles-test-byarticle-20181207.xml","data/articles-test-byarticle.jsonl")
 xml2json("../Webis-data/extracted/articles-training-bypublisher-20181122.xml","data/articles-training-bypublisher.jsonl")
